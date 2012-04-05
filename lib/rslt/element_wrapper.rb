0a72e68bde093b907adf5c8faf63259702db0151
Daniel Heath <daniel.r.heath@gmail.com>
2012-04-06 09:23:23 +1000

    Whitespace

--- lib/rslt/element_wrapper.rb
+++ lib/rslt/element_wrapper.rb
@@ -1,21 +1,21 @@
 module RSLT
   class ElementWrapper
-  
+
     attr_reader :element, :builder, :stylesheet
-  
+
     def initialize(stylesheet, element, builder)
       @stylesheet = stylesheet
       @element = element
       @builder = builder
     end
-  
+
     def child_content(selector=nil)
       elements = selector ? @element.css(selector) : @element.children
       elements.each do |child|
         stylesheet.generate_element(child)
       end
     end
-  
+
     def method_missing(sym, *args)
       begin
         @element.send(sym, *args)
@@ -23,15 +23,15 @@ module RSLT
         raise e.exception("Error delegating method '#{sym}' to #{@element.class.name}: #{e.message}\n\n#{e.backtrace.join("\n")}")
       end
     end
-  
+
     def add(*content)
       @builder << content.join('')
     end
-    
+
     def error(string)
       stylesheet.errors << string
     end
-        
+
   end
 
 end
--- spec/element_wrapper_spec.rb
+++ spec/element_wrapper_spec.rb
@@ -1,22 +1,22 @@
 require 'spec_helper'
 
 describe RSLT::ElementWrapper do
-  
+
   before do
     @stylesheet = Object.new
     @element = Object.new
     @builder = Object.new
     @wrapper = RSLT::ElementWrapper.new(@stylesheet, @element, @builder)
   end
-  
+
   describe "passing through method calls" do
     describe "when an error is raised in a passed through call" do
       it "should raise an error" do
         lambda {
           @wrapper.foo
-        }.should raise_exception      
+        }.should raise_exception
       end
     end
   end
-  
+
 end
